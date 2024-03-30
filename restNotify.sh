572b5277fb5e9f65c4cc43f0d90fde77502070c8
BeanVortex <47008296+BeanVortex@users.noreply.github.com>
2024-03-30 21:57:24 +0330

    Update restNotify.sh

--- restNotify.sh
+++ restNotify.sh
@@ -3,6 +3,6 @@ clear
 while [ "1" == "1" ]; do
     sleep 5s
     mplayer "$HOME/Music/notify.mp3" &
-    notify-send -t 2000 "RestMotherFucker"
+    notify-send -t 2000 "Rest Mister"
     sleep 10m
 done
